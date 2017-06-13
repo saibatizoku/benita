da9b0501d07dda1928508b106c3d35c221dc0ee5
Joaquín R <globojorro@gmail.com>
2017-06-12 19:26:25 -0500

    Defining commands for Temperature Chip (RTD EZO)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+/// Commands for RTD EZO Chip, taken from their Datasheet.
+enum RTD_EZO_COMMANDS {
+    Baud,
+    CalibrationTemperature,
+    CalibrationClear,
+    CalibrationState,
+    DataloggerPeriod,
+    DataloggerDisable,
+    DataloggerInterval,
+    DeviceAddress,
+    DeviceInformation,
+    Export,
+    ExportInfo,
+    Import,
+    Factory,
+    Find,
+    Lon,
+    Loff,
+    Lstate,
+    MemoryClear,
+    MemoryRecall,
+    MemoryRecallLastLocation,
+    ProtocolLockEnable,
+    ProtocolLockDisable,
+    ProtocolLockStatus,
+    Reading,
+    ScaleCelsius,
+    ScaleKelvin,
+    ScaleFahrenheit,
+    ScaleStatus,
+    Sleep,
+    Status
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
