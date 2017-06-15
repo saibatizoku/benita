593add9087b33d3d85e104e7820d0b73a41b62b4
Joaquín R <globojorro@gmail.com>
2017-06-15 12:43:29 -0500

    rename for clarity on arduino_blinker example

--- examples/arduino_blinker.rs
+++ examples/arduino_blinker.rs
@@ -9,16 +9,16 @@ use std::env;
 const ARDUINO_SLAVE_ADDR: u16 = 0x08;
 const I2CBUS_ID: u8 = 1;
 
-enum SlaveCommand {
-    BlinkerOn,
-    BlinkerOff,
+enum BlinkerCommand {
+    On,
+    Off,
 }
 
-impl I2cCommand for SlaveCommand {
+impl I2cCommand for BlinkerCommand {
     fn parse(&self) -> Vec<u8> {
         match self {
-            &SlaveCommand::BlinkerOn => vec![0x01],
-            &SlaveCommand::BlinkerOff => vec![0x00],
+            &BlinkerCommand::On => vec![0x01],
+            &BlinkerCommand::Off => vec![0x00],
         }
     }
 }
@@ -33,25 +33,29 @@ impl BlinkerService {
     }
     fn on(&self) -> Result<(), LinuxI2CError> {
         println!("Sending: Blink on");
-        self.device.send(SlaveCommand::BlinkerOn)
+        self.device.send(BlinkerCommand::On)
     }
     fn off(&self) -> Result<(), LinuxI2CError> {
         println!("Sending: Blink off");
-        self.device.send(SlaveCommand::BlinkerOff)
+        self.device.send(BlinkerCommand::Off)
     }
 }
 
 /// This is the main program. It takes one argument: if it is 'on', the I2C
-/// slave will be commanded to turn on blinking on the LED.
-/// Anything else will turn it off.
+/// slave will be commanded to turn on blinking on the LED. 'off' will turn
+/// the blinking off.
+///
+/// Anything else will exit normally, printing a message.
+///
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
     let arduino = SlaveDevice::new(I2CBUS_ID, ARDUINO_SLAVE_ADDR);
     let blinker = BlinkerService::new(arduino.clone());
     match &command[..] {
-        "on" => blinker.on().unwrap(),
-        "off" | _ => blinker.off().unwrap(),
+        "on"  => blinker.on().unwrap(),
+        "off" => blinker.off().unwrap(),
+        _     => println!("'{}' doesn't exist. Use 'on' or 'off'.", command),
     };
     ::std::process::exit(0);
 }
