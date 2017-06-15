8cfe84c40bfab0a2d25fede765d2e4e4caaa1bc9
Joaquín R <globojorro@gmail.com>
2017-06-15 00:33:33 -0500

    I2C slave abstractions

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,34 @@
 //! * pH
 //! * Electrical Conductivity
 //! * Temperature
-pub mod sensors;
+extern crate i2cdev;
+
+mod sensors;
+
+use i2cdev::core::I2CDevice;
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
+pub struct SlaveDevice {
+    bus: u8,
+    address: u16,
+}
+
+pub trait I2cCommand {
+    fn parse(&self) -> Vec<u8>;
+}
+
+pub trait I2cSlave {
+    fn new(bus: u8, address: u16) -> Self;
+    fn send<T: I2cCommand>(&self, cmd: T) -> Result<(), LinuxI2CError>;
+}
+
+impl I2cSlave for SlaveDevice {
+    fn new(bus: u8, address: u16) -> SlaveDevice {
+        SlaveDevice { bus, address }
+    }
+    fn send<T: I2cCommand>(&self, cmd: T) -> Result<(), LinuxI2CError> {
+        let bus = format!("/dev/i2c-{}", self.bus);
+        let mut dev = try!(LinuxI2CDevice::new(bus, self.address));
+        dev.write(&cmd.parse())
+    }
+}
