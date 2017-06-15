2ac5048a54386271f2734673bff2150dd8f3efad
Joaquín R <globojorro@gmail.com>
2017-06-14 21:17:23 -0500

    further refactoring of i2c example with traits

--- examples/arduino_blinker.rs
+++ examples/arduino_blinker.rs
@@ -1,45 +1,54 @@
+extern crate benita;
 extern crate i2cdev;
 
+use benita::I2cCommand;
 use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 use std::env;
 
 const ARDUINO_SLAVE_ADDR: u16 = 0x08;
+const I2CBUS_ID: u8 = 1;
 
 enum ArduinoCommand {
     BlinkerOn,
     BlinkerOff,
 }
 
-trait I2cSlave {
-    fn parse(&self) -> u8;
-    fn send(&self) -> Result<(), LinuxI2CError>;
+struct ArduinoDevice {
+    bus: u8,
+    address: u16,
 }
 
-impl I2cSlave for ArduinoCommand {
+impl ArduinoDevice {
+    pub fn new(bus: u8, address: u16) -> ArduinoDevice {
+        ArduinoDevice { bus, address }
+    }
+    pub fn send(&self, cmd: ArduinoCommand) -> Result<(), LinuxI2CError> {
+        let bus = format!("/dev/i2c-{}", self.bus);
+        let mut dev = try!(LinuxI2CDevice::new(bus, self.address));
+        dev.smbus_write_byte(cmd.parse())
+    }
+}
+
+impl I2cCommand for ArduinoCommand {
     fn parse(&self) -> u8 {
         match self {
             &ArduinoCommand::BlinkerOn => 0x01,
             &ArduinoCommand::BlinkerOff => 0x00,
         }
     }
-    fn send(&self) -> Result<(), LinuxI2CError> {
-        let i2cbus = format!("/dev/i2c-{}", 1);
-        let mut dev = try!(LinuxI2CDevice::new(i2cbus, ARDUINO_SLAVE_ADDR));
-        dev.smbus_write_byte(self.parse())
-    }
 }
 
-fn blinker_on() {
+fn blinker_on(dev: &ArduinoDevice) {
     println!("Sending: Blink on");
-    ArduinoCommand::BlinkerOn.send().unwrap();
+    dev.send(ArduinoCommand::BlinkerOn).unwrap();
 }
 
 
-fn blinker_off() {
+fn blinker_off(dev: &ArduinoDevice) {
     println!("Sending: Blink off");
-    ArduinoCommand::BlinkerOff.send().unwrap();
+    dev.send(ArduinoCommand::BlinkerOff).unwrap();
 }
 
 /// This is the main program. It takes one argument: if it is 'on', the I2C
@@ -48,9 +57,10 @@ fn blinker_off() {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
+    let device = ArduinoDevice::new(I2CBUS_ID, ARDUINO_SLAVE_ADDR);
     match &command[..] {
-        "on" => blinker_on(),
-        _ => blinker_off(),
+        "on" => blinker_on(&device),
+        _ => blinker_off(&device),
     };
     ::std::process::exit(0);
 }
