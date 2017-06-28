3a39ea371f9d7efd992a3d2e2afc27dcf2bc8b5b
Joaquín R <globojorro@gmail.com>
2017-06-27 23:24:38 -0500

    use error chain in ezo example

--- examples/ezo-sensor.rs
+++ examples/ezo-sensor.rs
@@ -1,23 +1,27 @@
+#![recursion_limit = "1024"]
+extern crate benita;
 extern crate i2cdev;
+
+use benita::errors::*;
+use i2cdev::core::*;
+use i2cdev::linux::LinuxI2CDevice;
 
 use std::ascii::AsciiExt;
 use std::thread;
 use std::time::Duration;
 
-use i2cdev::core::*;
-use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
-
 const DEVICE_ID: u8 = 1;
 const EZO_SLAVE_ADDR: u16 = 101; // could be specified as 0x65
 
 // real code should probably not use unwrap()
-fn i2cfun(cmd: &str, delay: u64) -> Result<(), LinuxI2CError> {
+fn i2cfun(cmd: &str, delay: u64) -> Result<()> {
     let device_path = format!("/dev/i2c-{}", DEVICE_ID);
-    let mut dev = try!(LinuxI2CDevice::new(&device_path, EZO_SLAVE_ADDR));
+    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SLAVE_ADDR)
+        .chain_err(|| "Could not open I2C device")?;
     println!("I2C Device opened at {}", &device_path);
 
     println!("Sending command: '{}'", cmd.to_ascii_uppercase());
-    try!(dev.write(cmd.as_bytes()));
+    dev.write(cmd.as_bytes()).chain_err(|| "Command could not be sent")?;
     println!("I2C Command sent");
 
     thread::sleep(Duration::from_millis(delay));
@@ -43,12 +47,31 @@ fn i2cfun(cmd: &str, delay: u64) -> Result<(), LinuxI2CError> {
     Ok(())
 }
 
+/// This is the main program, it executes `run_command` with error-chain.
+fn main() {
+    if let Err(ref e) = run_command() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+        ::std::process::exit(1);
+    }
+}
+
 /// This is the main code. It sends the command `cmd` and waits `delay` millis
 /// before reading the response.
-fn main() {
+fn run_command() -> Result<()> {
     let cmd = "r\0";
     match i2cfun(&cmd, 600) {
         Ok(_) => println!("I2C command sent; Response processed"),
         Err(e) => println!("Bad luck, I2C command failed: {:?}", e),
-    }
+    };
+    Ok(())
 }
