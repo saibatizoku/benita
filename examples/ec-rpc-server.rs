2e576efe6edc17b8f9c5e376429f1d502cb9bc99
Joaquín R <globojorro@gmail.com>
2017-07-26 01:09:40 -0500

    add comments to ec-rpc-server example

--- examples/ec-rpc-server.rs
+++ examples/ec-rpc-server.rs
@@ -23,10 +23,14 @@ const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum PossibleCommand {
+    // 'O,?' command
     GetParams,
-    NotRecognized,
+    // 'R'
     Read,
+    // 'SLEEP' command
     Sleep,
+    // command not recognized
+    NotRecognized,
 }
 
 fn parse_command(cmd_str: &str) -> PossibleCommand {
@@ -39,27 +43,37 @@ fn parse_command(cmd_str: &str) -> PossibleCommand {
 }
 
 fn run() -> Result<()> {
+    // We initialize our I2C device connection.
     let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
     let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
         .chain_err(|| "Could not open I2C device")?;
 
+    // We start our ZMQ context.
     let context = zmq::Context::new();
+    // We configure our socket as REP, for accepting requests
+    // and providing REsPonses.
     let responder = context.socket(zmq::REP).unwrap();
-
+    // We bind our socket to local port 5557, using TCP.
     assert!(responder.bind("tcp://*:5557").is_ok());
-
+    // We initialize our ZMQ message. It will be reused throughout.
     let mut msg = zmq::Message::new().unwrap();
+
+    // This is the main loop, it will run for as long as the program runs.
     loop {
+        // We start by recieving the command request from the client.
         responder.recv(&mut msg, 0).unwrap();
+
+        // The command as a str.
         let msg_str = msg.as_str().unwrap();
-        println!("Received {}", msg_str);
+
+        // Parse and process the command.
         let _command_response = match parse_command(msg_str) {
             PossibleCommand::NotRecognized => {
                 "Unknown command".to_string()
             }
             PossibleCommand::GetParams => {
                 let output_state = ec_command::OutputState.run(&mut dev)?;
-                format!("{}", output_state.to_string())
+                output_state.to_string()
             }
             PossibleCommand::Read => {
                 let sensor_output = ec_command::Reading.run(&mut dev)?;
@@ -69,8 +83,12 @@ fn run() -> Result<()> {
                 "Sleeping".to_string()
             }
         };
-        //thread::sleep(Duration::from_millis(1));
+
+        // Send response to the client.
         responder.send(_command_response.as_bytes(), 0).unwrap();
+
+        // No work left, so we sleep.
+        thread::sleep(Duration::from_millis(1));
     }
 }
 
