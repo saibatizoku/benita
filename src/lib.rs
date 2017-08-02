a35d9ce5998da9144f1ee4fa09ecd315febf8117
Joaquín R <globojorro@gmail.com>
2017-08-02 17:44:32 -0500

    read n parse TOML config files

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,9 @@
 #[macro_use]
 extern crate error_chain;
 extern crate i2cdev;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
 extern crate url;
 extern crate zmq;
 
@@ -49,5 +52,77 @@ impl I2cSensing for SensingDevice {
         let mut dev = LinuxI2CDevice::new(bus, self.address)
                        .chain_err(|| "Could not open I2C device")?;
         dev.write(&cmd.to_bytes()).chain_err(|| "Could not send command")
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+pub struct Config<'a> {
+    pub pub_url: &'a str,
+    pub channel: &'a str,
+    pub rep_url: &'a str,
+}
+
+impl<'a> Config<'a> {
+    pub fn from_str(config_str: &str) -> Result<Config> {
+        toml::from_str(config_str)
+                    .chain_err(|| ErrorKind::ConfigParse)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reads_and_parses_config_files() {
+
+        // Files with correct fields parse
+        let config_str = r#"
+            pub_url = "tcp://127.0.0.1:5558"
+            channel = "temperature-0123456789abcdef"
+            rep_url = "tcp://127.0.0.1:5557"
+            "#;
+
+        let config = Config::from_str(config_str).unwrap();
+        assert_eq!(config, Config {
+            pub_url: "tcp://127.0.0.1:5558",
+            channel: "temperature-0123456789abcdef",
+            rep_url: "tcp://127.0.0.1:5557",
+        });
+
+        // Unknown fields are ignored
+        let config_str = r#"
+            pub_url = "tcp://localhost:5558"
+            channel = "temperature-0123456789abcdef"
+            rep_url = "tcp://localhost:5557"
+            proxy_url = "tcp://localhost:5550"
+            another_extra = "yeah"
+            "#;
+
+        let config = Config::from_str(config_str).unwrap();
+        assert_eq!(config, Config {
+            pub_url: "tcp://localhost:5558",
+            channel: "temperature-0123456789abcdef",
+            rep_url: "tcp://localhost:5557",
+        });
+    }
+
+    #[test]
+    fn reads_and_parses_invalid_config_files_yielding_err() {
+        // Files with no known fields yield error
+        let config_str = r#""#;
+
+        let config: Result<Config> = Config::from_str(config_str);
+        assert!(config.is_err());
+
+        // Files with no known fields yield error
+        let config_str = r#"
+            pub_url = "tcp://192.168.16.123:5558"
+            channel = "temperature-0123456789abcdef"
+            rep_url = 1234
+            "#;
+
+        let config: Result<Config> = Config::from_str(config_str);
+        assert!(config.is_err());
     }
 }
