8295ad1bc116ed200b505a7f03dd6a360533a5b7
Joaquin Rosales <globojorro@gmail.com>
2017-09-07 01:16:29 -0500

    add missing docs

--- src/sensors/ezo_rtd.rs
+++ src/sensors/ezo_rtd.rs
@@ -60,6 +60,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Set the calibration temperature for the sensor.
     pub fn set_calibration_temperature(&mut self, t: f64) -> Result<()> {
         let _cmd = CalibrationTemperature(t)
             .run(&mut self.i2cdev)
@@ -67,6 +68,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Clear the device's calibration settings.
     pub fn set_calibration_clear(&mut self) -> Result<()> {
         let _cmd = CalibrationClear
             .run(&mut self.i2cdev)
@@ -74,6 +76,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Get the sensor's current calibration settings.
     pub fn get_calibration_status(&mut self) -> Result<CalibrationStatus> {
         let cal = CalibrationState
             .run(&mut self.i2cdev)
@@ -81,6 +84,8 @@ impl TemperatureSensor {
         Ok(cal)
     }
 
+    /// Get a summary of the number of calibration strings required to export the current sensor
+    /// settings. It includes the number of lines and the total sum of exportable characters.
     pub fn get_export_info(&mut self) -> Result<ExportedInfo> {
         let info = ExportInfo
             .run(&mut self.i2cdev)
@@ -88,6 +93,10 @@ impl TemperatureSensor {
         Ok(info)
     }
 
+    /// Get a single calibration string from the device. This command needs to be called
+    /// repeatedly, use the function `get_export_info()` to find out how many times.
+    ///
+    /// When the device has conclue
     pub fn get_export_line(&mut self) -> Result<Exported> {
         let export = Export
             .run(&mut self.i2cdev)
@@ -95,6 +104,7 @@ impl TemperatureSensor {
         Ok(export)
     }
 
+    /// Import a calibration string to the device.
     pub fn set_import_line(&mut self, import: String) -> Result<()> {
         let _import = Import(import)
             .run(&mut self.i2cdev)
@@ -102,6 +112,9 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Set the data logger interval, `n`.
+    ///
+    /// The device will take readings and save them to memory at the given interval.
     pub fn set_data_logger_interval(&mut self, n: u32) -> Result<()> {
         let _set = DataloggerPeriod(n)
             .run(&mut self.i2cdev)
@@ -109,6 +122,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Disable the data-logger.
     pub fn set_data_logger_off(&mut self) -> Result<()> {
         let _set = DataloggerDisable
             .run(&mut self.i2cdev)
@@ -116,6 +130,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Get the current status of the data-logger.
     pub fn get_data_logger_status(&mut self) -> Result<DataLoggerStorageIntervalSeconds> {
         let interval = DataloggerInterval
             .run(&mut self.i2cdev)
@@ -123,6 +138,9 @@ impl TemperatureSensor {
         Ok(interval)
     }
 
+    /// Set the sensor device to the factory settings.
+    ///
+    /// __NOTE:__ this will delete the settings of the device.
     pub fn set_factory_reset(&mut self) -> Result<()> {
         let _reset = Factory
             .run(&mut self.i2cdev)
@@ -130,12 +148,18 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Set the device on Find mode. This will make the LED blink continuously until the device
+    /// receives a new command.
     pub fn set_find_mode(&mut self) -> Result<()> {
         let _find = Find.run(&mut self.i2cdev)
             .chain_err(|| ErrorKind::SensorTrouble)?;
         Ok(())
     }
 
+    /// Set a new I2C address on the device.
+    ///
+    /// __NOTE:__ using this command will make the current `self` obsolete. It is up to you to
+    /// create a new `TemperatureSensor` that is properly configured.
     pub fn set_i2c_address(&mut self, address: u16) -> Result<()> {
         let _set = DeviceAddress(address)
             .run(&mut self.i2cdev)
@@ -143,6 +167,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Get the general information about the sensor device.
     pub fn get_device_info(&mut self) -> Result<DeviceInfo> {
         let info = DeviceInformation
             .run(&mut self.i2cdev)
@@ -150,6 +175,7 @@ impl TemperatureSensor {
         Ok(info)
     }
 
+    /// Turn off the LED.
     pub fn set_led_off(&mut self) -> Result<()> {
         let _set = LedOff
             .run(&mut self.i2cdev)
@@ -157,6 +183,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Turn on the LED.
     pub fn set_led_on(&mut self) -> Result<()> {
         let _set = LedOn
             .run(&mut self.i2cdev)
@@ -164,6 +191,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Get the current status of the LED.
     pub fn get_led_status(&mut self) -> Result<LedStatus> {
         let status = LedState
             .run(&mut self.i2cdev)
@@ -171,6 +199,7 @@ impl TemperatureSensor {
         Ok(status)
     }
 
+    /// Clear memory readings.
     pub fn set_memory_clear(&mut self) -> Result<()> {
         let _set = MemoryClear
             .run(&mut self.i2cdev)
@@ -178,6 +207,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Recall the next memory reading on the stack.
     pub fn get_memory_recall(&mut self) -> Result<MemoryReading> {
         let reading = MemoryRecall
             .run(&mut self.i2cdev)
@@ -185,6 +215,7 @@ impl TemperatureSensor {
         Ok(reading)
     }
 
+    /// Recall the last memory reading on the stack.
     pub fn get_memory_recall_last(&mut self) -> Result<MemoryReading> {
         let reading = MemoryRecallLast
             .run(&mut self.i2cdev)
@@ -192,6 +223,7 @@ impl TemperatureSensor {
         Ok(reading)
     }
 
+    /// Set the lock off for the I2C protocol mode.
     pub fn set_protocol_lock_off(&mut self) -> Result<()> {
         let _set = ProtocolLockDisable
             .run(&mut self.i2cdev)
@@ -199,6 +231,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Set the lock on for the I2C protocol mode.
     pub fn set_protocol_lock_on(&mut self) -> Result<()> {
         let _set = ProtocolLockEnable
             .run(&mut self.i2cdev)
@@ -206,6 +239,7 @@ impl TemperatureSensor {
         Ok(())
     }
 
+    /// Get the I2C protocol mode status.
     pub fn get_protocol_lock_status(&mut self) -> Result<ProtocolLockStatus> {
         let status = ProtocolLockState
             .run(&mut self.i2cdev)
@@ -254,6 +288,11 @@ impl TemperatureSensor {
     }
 
     /// Set the sensor chip to sleep.
+    ///
+    /// __NOTE:__ using this command will make the sensor device sleep until:
+    ///
+    /// 1.  it is woken up by writing a single byte to the device, or
+    /// 2.   by sending __any__ valid command.
     pub fn set_sleep(&mut self) -> Result<()> {
         let _sleep = Sleep
             .run(&mut self.i2cdev)
