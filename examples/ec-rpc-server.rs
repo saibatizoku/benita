253a25989f3b64033f47cac41acb69551f2e1014
Joaquin Rosales <globojorro@gmail.com>
2017-09-09 00:57:23 -0500

    refactor examples to use ConductivitySensor instead of raw i2c calls

--- examples/ec-rpc-server.rs
+++ examples/ec-rpc-server.rs
@@ -6,22 +6,18 @@
 #![recursion_limit = "1024"]
 
 extern crate benita;
-extern crate chrono;
 extern crate clap;
-extern crate ezo_ec;
-extern crate i2cdev;
 extern crate neuras;
 
 use std::thread;
 use std::time::Duration;
 
 use benita::errors::{Result, ResultExt};
+use benita::sensors::ezo_ec::ConductivitySensor;
+use benita::sensors::ezo_ec::responses::OutputStringStatus;
+
 use clap::{App, Arg};
-use ezo_ec::command as ec_command;
-use ezo_ec::response as ec_response;
-use ec_command::Command;
-use ec_response::OutputStringStatus;
-use i2cdev::linux::LinuxI2CDevice;
+use neuras::utils::bind_socket;
 
 const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
@@ -88,7 +84,7 @@ fn parse_cli_arguments() -> Result<()> {
 fn run(rep_url: &str) -> Result<()> {
     // We initialize our I2C device connection.
     let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
-    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
+    let mut ec_sensor = ConductivitySensor::new(&device_path, EZO_SENSOR_ADDR)
         .chain_err(|| "Could not open I2C device")?;
 
     // We start our ZMQ context.
@@ -97,7 +93,8 @@ fn run(rep_url: &str) -> Result<()> {
     // and providing REsPonses.
     let responder = neuras::utils::zmq_rep(&context)?;
     // We bind our socket to REP_URL.
-    assert!(responder.bind(rep_url).is_ok());
+    let _bind_socket = bind_socket(&responder, rep_url)
+        .chain_err(|| "problems binding to socket")?;
     // We initialize our ZMQ message. It will be reused throughout.
     let mut msg = neuras::utils::create_message()?;
 
@@ -112,20 +109,20 @@ fn run(rep_url: &str) -> Result<()> {
         // Parse and process the command.
         let command_response = match parse_command(msg_str) {
             PossibleCommand::Calibrate(temp) => {
-                let _calibrate = ec_command::TemperatureCompensation(temp).run(&mut dev)?;
+                let _calibrate = ec_sensor.set_compensation_temperature(temp)?;
                 format!("Compensated Temperature: {}", temp)
             }
             PossibleCommand::NotRecognized => "Unknown command".to_string(),
             PossibleCommand::GetParams => {
-                let output_state: OutputStringStatus = ec_command::OutputState.run(&mut dev)?;
+                let output_state: OutputStringStatus = ec_sensor.get_output_string_status()?;
                 output_state.to_string()
             }
             PossibleCommand::Read => {
-                let sensor_output = ec_command::Reading.run(&mut dev)?;
+                let sensor_output = ec_sensor.get_reading()?;
                 format!("{:?}", sensor_output)
             }
             PossibleCommand::Sleep => {
-                let _sleep = ec_command::Sleep.run(&mut dev)?;
+                let _sleep = ec_sensor.set_sleep()?;
                 "Sleeping".to_string()
             }
         };
