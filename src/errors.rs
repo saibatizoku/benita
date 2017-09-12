b3ef206f46ace03224b8becd726540677cbed06d
Joaquin Rosales <globojorro@gmail.com>
2017-09-11 19:03:21 -0500

    add ezo_common to our error-chain

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
 //! Errors.
 use std::io;
 use neuras;
+use ezo_common;
 use ezo_ec;
 use ezo_ph;
 use ezo_rtd;
@@ -25,6 +26,7 @@ error_chain! {
     }
     links {
         Neuras(neuras::errors::Error, neuras::errors::ErrorKind);
+        EzoSensor(ezo_common::errors::Error, ezo_common::errors::ErrorKind);
         EcSensor(ezo_ec::errors::Error, ezo_ec::errors::ErrorKind);
         PhSensor(ezo_ph::errors::Error, ezo_ph::errors::ErrorKind);
         RtdSensor(ezo_rtd::errors::Error, ezo_rtd::errors::ErrorKind);
