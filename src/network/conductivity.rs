47da77412dc41e3ab6b836bf593c7888c1c828bd
Joaquin Rosales <globojorro@gmail.com>
2017-09-27 12:34:56 -0500

    use macros to define conductivity network server/client

--- src/network/conductivity.rs
+++ src/network/conductivity.rs
@@ -34,14 +34,50 @@ impl REPCommand {
     }
 }
 
+sensor_socket!(ConductivityClient, SocketSend,
+               "Socket that communicates with the Conductivity sensor.");
 
-        }
-        }
+impl ConductivityClient {
+    pub fn get_output_params(&mut self) -> Result<String> {
+        let _read = self.send("get_params".as_bytes())?;
+        let _response = self.recv()?;
+        match _response.as_str() {
+            Some(resp) => Ok(resp.to_string()),
+            _ => Err(ErrorKind::SocketReceive.into()),
         }
     }
 
+    pub fn send_compensate(&mut self, t: f64) -> Result<String> {
+        let calibrate = format!("calibrate {:.*}", 3, t);
+        let _read = self.send(calibrate.as_bytes())?;
+        let _response = self.recv()?;
+        match _response.as_str() {
+            Some(resp) => Ok(resp.to_string()),
+            _ => Err(ErrorKind::SocketReceive.into()),
+        }
+    }
+
+    pub fn send_read(&mut self) -> Result<String> {
+        let _read = self.send("read".as_bytes())?;
+        let _response = self.recv()?;
+        match _response.as_str() {
+            Some(reading) => Ok(reading.to_string()),
+            _ => Err(ErrorKind::SocketReceive.into()),
+        }
+    }
+
+    pub fn send_sleep(&mut self) -> Result<String> {
+        let _read = self.send("read".as_bytes())?;
+        let _response = self.recv()?;
+        match _response.as_str() {
+            Some(reading) => Ok(reading.to_string()),
+            _ => Err(ErrorKind::SocketReceive.into()),
+        }
+    }
 }
 
+sensor_socket!(ConductivityServer, SocketSend,
+               "Socket that responds to Conductivity sensor commands.");
 
     }
 }
