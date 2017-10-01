70febff078ae3f248cf7905d5e5cc6dddd25be70
Joaquin Rosales <globojorro@gmail.com>
2017-10-01 18:34:26 -0500

    update ph client code

--- src/network/ph/mod.rs
+++ src/network/ph/mod.rs
@@ -11,38 +11,30 @@ network_socket!(PhClient,
 impl PhClient {
     pub fn get_output_params(&mut self) -> Result<String> {
         let _read = self.send("get_params".as_bytes())?;
-        let _response = self.recv()?;
-        match _response.as_str() {
-            Some(resp) => Ok(resp.to_string()),
-            _ => Err(ErrorKind::CommandResponse.into()),
-        }
+        let response = self.recv()
+            .chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
     }
 
     pub fn send_compensate(&mut self, t: f64) -> Result<String> {
         let calibrate = format!("calibrate {:.*}", 3, t);
         let _read = self.send(calibrate.as_bytes())?;
-        let _response = self.recv()?;
-        match _response.as_str() {
-            Some(resp) => Ok(resp.to_string()),
-            _ => Err(ErrorKind::CommandResponse.into()),
-        }
+        let response = self.recv()
+            .chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
     }
 
     pub fn send_read(&mut self) -> Result<String> {
         let _read = self.send("read".as_bytes())?;
-        let _response = self.recv()?;
-        match _response.as_str() {
-            Some(reading) => Ok(reading.to_string()),
-            _ => Err(ErrorKind::CommandResponse.into()),
-        }
+        let response = self.recv()
+            .chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
     }
 
     pub fn send_sleep(&mut self) -> Result<String> {
         let _read = self.send("read".as_bytes())?;
-        let _response = self.recv()?;
-        match _response.as_str() {
-            Some(reading) => Ok(reading.to_string()),
-            _ => Err(ErrorKind::CommandResponse.into()),
-        }
+        let response = self.recv()
+            .chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
     }
 }
