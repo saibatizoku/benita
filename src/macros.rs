4c32d6dd3a6cd1c36981a376645e0708d065d053
Joaquin Rosales <globojorro@gmail.com>
2017-10-01 18:33:03 -0500

    add errors to macro code

--- src/macros.rs
+++ src/macros.rs
@@ -21,7 +21,7 @@ macro_rules! sensor_i2cdev {
             pub fn new(i2c_path: &str, i2c_address: u16) -> Result<$name> {
                 let i2cdev = LinuxI2CDevice::new(i2c_path, i2c_address)
                     .chain_err(|| ErrorKind::SensorTrouble)?;
-                Ok($name { i2cdev: i2cdev })
+                Ok( $name { i2cdev: i2cdev } )
             }
         }
     };
@@ -34,14 +34,12 @@ macro_rules! network_socket {
         #[ doc = $doc ]
         pub struct $name {
             socket: neuras::zmq::Socket,
-            message: neuras::zmq::Message,
         }
 
         impl $name {
             /// Create a new network socket.
             pub fn new(socket: neuras::zmq::Socket) -> Result<$name> {
-                let message = neuras::utils::create_message()?;
-                Ok($name { socket, message })
+                Ok( $name { socket } )
             }
         }
 
@@ -51,15 +49,13 @@ macro_rules! network_socket {
         #[ doc = $doc ]
         pub struct $name {
             socket: neuras::zmq::Socket,
-            message: neuras::zmq::Message,
             sensor: $sensor,
         }
 
         impl $name {
             /// Create a new network socket.
             pub fn new(socket: neuras::zmq::Socket, sensor: $sensor) -> Result<$name> {
-                let message = neuras::utils::create_message()?;
-                Ok($name { socket, message, sensor })
+                Ok( $name { socket, sensor } )
             }
         }
 
@@ -92,48 +88,15 @@ macro_rules! network_socket_impl {
             }
 
             /// Receives a message from the network socket.
-            pub fn recv(&mut self) -> Result<&neuras::zmq::Message> {
-                let _recv = self.socket.recv(&mut self.message, 0)
+            pub fn recv(&self) -> Result<String> {
+                let received_result = self.socket.recv_string(0)
                     .chain_err(|| ErrorKind::SocketReceive)?;
-                Ok(&self.message)
-            }
-        }
-
-    };
-    ($name:ident) => {
-        impl $name {
-            /// Create a new network socket.
-            pub fn new(socket: neuras::zmq::Socket) -> Result<$name> {
-                let message = neuras::utils::create_message()?;
-                Ok($name { socket, message })
-            }
-
-            /// Binds the socket to the given URL.
-            pub fn bind(&self, url: &str) -> Result<()> {
-                let _bind = neuras::utils::bind_socket(&self.socket, url)
-                    .chain_err(|| ErrorKind::SocketBind)?;
-                Ok(())
-            }
-
-            /// Connects the socket to the given URL.
-            pub fn connect(&self, url: &str) -> Result<()> {
-                let _connect = neuras::utils::connect_socket(&self.socket, url)
-                    .chain_err(|| ErrorKind::SocketConnect)?;
-                Ok(())
-            }
-
-            /// Sends a message over the network socket.
-            pub fn send(&self, msg: &[u8]) -> Result<()> {
-                let _send = self.socket.send(msg, 0)
-                    .chain_err(|| ErrorKind::SocketSend)?;
-                Ok(())
-            }
-
-            /// Receives a message from the network socket.
-            pub fn recv(&mut self) -> Result<&neuras::zmq::Message> {
-                let _recv = self.socket.recv(&mut self.message, 0)
-                    .chain_err(|| ErrorKind::SocketReceive)?;
-                Ok(&self.message)
+                // We match against the resulting `Resulṭ<String, Vec<u8>>`
+                let response = match received_result {
+                    Ok(r) => r,
+                    _ => return Err(ErrorKind::ResponseParse.into()),
+                };
+                Ok(response)
             }
         }
     };
