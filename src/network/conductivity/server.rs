af40b5f6c6f7d72b24686c167f9c6228cd605d05
Joaquin Rosales <globojorro@gmail.com>
2017-10-08 17:35:43 -0500

    trim some fat off network conductivity server

--- src/network/conductivity/server.rs
+++ src/network/conductivity/server.rs
@@ -1,80 +1,11 @@
 //! Server for Conductivity sensing.
-pub mod commands {
-    use errors::*;
-    use super::{ConductivitySensorServer, SocketCommand};
-
-    macro_rules! conductivity_command {
-        ( $name:ident , $response:ty ,
-          $resp:ident : $runfn:block,
-          $doc:tt ) => {
-              socket_command! {
-                  $name, SocketCommand,
-                  ConductivitySensorServer,
-                  response : $response,
-                  $resp : $runfn,
-                  $doc
-              }
-          };
-    }
-
-    conductivity_command! {
-        ReadCommand, String,
-        responder: {
-            let response = responder.sensor.get_reading()
-                .chain_err(|| ErrorKind::CommandRequest)?;
-            Ok(format!("{:?}", response))
-        },
-        "Read command"
-    }
-
-    conductivity_command! {
-        SleepCommand, (),
-        responder: {
-            let _response = responder.sensor.set_sleep()
-                .chain_err(|| ErrorKind::CommandRequest)?;
-            Ok(())
-        },
-        "Network command for sleep mode."
-    }
-}
-
 use errors::*;
 use network::SocketCommand;
 use sensors::conductivity::ConductivitySensor;
-use self::commands::{ReadCommand, SleepCommand};
 
 use neuras;
 
-/// REP command-set.
-///
-/// *   `calibrate n` command, where n is a temperature float/int.
-/// *   `get_params` command, return the output readings configuration.
-/// *   `read` command, returns the output readings.
-/// *   `sleep` command, sets the device to sleep/low-power mode.
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum REPCommand {
-    Calibrate(f64),
-    GetParams,
-    Read,
-    Sleep,
-}
 
-impl REPCommand {
-    pub fn parse(cmd_str: &str) -> Result<REPCommand> {
-        let cmd = match cmd_str {
-            "read" => REPCommand::Read,
-            a if cmd_str.starts_with("calibrate ") => {
-                let rest = a.get(10..).unwrap();
-                let temp = rest.parse().unwrap();
-                REPCommand::Calibrate(temp)
-            }
-            "get_params" => REPCommand::GetParams,
-            "sleep" => REPCommand::Sleep,
-            _ => return Err(ErrorKind::CommandParse.into()),
-        };
-        Ok(cmd)
-    }
-}
 // Define the network server socket for directly interacting with the
 // Conductivity sensor via I2C.
 network_socket! {
@@ -84,37 +15,20 @@ network_socket! {
 }
 
 impl ConductivitySensorServer {
-    pub fn process_request(&mut self, cli_command: REPCommand) -> Result<String> {
-        // Parse and process the command.
-        let parsed = match cli_command {
-            REPCommand::Calibrate(temp) => match self.set_compensation(temp) {
-                Ok(_) => format!("temperature-compensation {}", temp),
-                Err(e) => format!("error {}", e),
-            },
-            REPCommand::GetParams => match self.get_output_params() {
-                Ok(output_state) => output_state.to_string(),
-                Err(e) => format!("error {}", e),
-            },
-            REPCommand::Read => match self.get_reading() {
-                Ok(sensor_output) => format!("{:?}", sensor_output),
-                Err(e) => format!("error {}", e),
-            },
-            REPCommand::Sleep => match self.set_sleep() {
-                Ok(_) => "sleeping".to_string(),
-                Err(e) => format!("error {}", e),
-            },
-        };
-        Ok(parsed)
+    /// get the compensation temperature for sensor readings.
+    pub fn get_compensation(&mut self) -> Result<String> {
+        let response = self.sensor
+            .get_compensated_temperature_value()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok(format!("compensation value: {}", response.0))
     }
-}
 
-impl ConductivitySensorServer {
     /// set the compensation temperature for sensor readings.
     pub fn set_compensation(&mut self, t: f64) -> Result<String> {
         let _response = self.sensor
             .set_compensation_temperature(t)
             .chain_err(|| ErrorKind::CommandRequest)?;
-        Ok(format!("temperature-compensated {}", t))
+        Ok(format!("compensation set: {}", t))
     }
 }
 
@@ -126,20 +40,88 @@ impl ConductivitySensorServer {
             .chain_err(|| ErrorKind::CommandRequest)?;
         Ok(response.to_string())
     }
+
+    /// set the `ec` output string parameter on.
+    pub fn set_output_conductivity_on(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_conductivity_on()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output ec: on".to_string())
+    }
+
+    /// set the `ec` output string parameter on.
+    pub fn set_output_conductivity_off(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_conductivity_off()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output ec: off".to_string())
+    }
+
+    /// set the `salinity` output string parameter on.
+    pub fn set_output_salinity_on(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_salinity_on()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output salinity: on".to_string())
+    }
+
+    /// set the `salinity` output string parameter on.
+    pub fn set_output_salinity_off(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_salinity_off()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output salinity: off".to_string())
+    }
+
+    /// set the `sg` output string parameter on.
+    pub fn set_output_specific_gravity_on(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_specific_gravity_on()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output sg: on".to_string())
+    }
+
+    /// set the `sg` output string parameter on.
+    pub fn set_output_specific_gravity_off(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_specific_gravity_off()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output sg: off".to_string())
+    }
+
+    /// set the `tds` output string parameter on.
+    pub fn set_output_tds_on(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_tds_on()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output tds: on".to_string())
+    }
+
+    /// set the `tds` output string parameter on.
+    pub fn set_output_tds_off(&mut self) -> Result<String> {
+        let _response = self.sensor
+            .set_output_tds_off()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok("output tds: off".to_string())
+    }
 }
 
 impl ConductivitySensorServer {
     /// get the output string with sensor readings.
     pub fn get_reading(&mut self) -> Result<String> {
-        let response = ReadCommand.run(self)?;
-        Ok(response)
+        let response = self.sensor
+            .get_reading()
+            .chain_err(|| ErrorKind::CommandRequest)?;
+        Ok(format!("{:?}", response))
     }
 }
 
 impl ConductivitySensorServer {
     /// set the sensor to sleep (low-power) mode.
     pub fn set_sleep(&mut self) -> Result<String> {
-        let _sleep = SleepCommand.run(self)?;
+        let _sleep = self.sensor
+            .set_sleep()
+            .chain_err(|| ErrorKind::CommandRequest)?;
         Ok("sleeping".to_string())
     }
 }
