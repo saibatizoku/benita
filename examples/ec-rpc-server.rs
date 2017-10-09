ec960eb6afa953d0c73569fedfcbcee709539fe5
Joaquin Rosales <globojorro@gmail.com>
2017-10-08 22:12:10 -0500

    update ec-rpc-server example

--- examples/ec-rpc-server.rs
+++ examples/ec-rpc-server.rs
@@ -8,8 +8,9 @@
 extern crate benita;
 extern crate clap;
 
+use benita::cli::shared::is_url;
 use benita::errors::*;
-use benita::services::run_conductivity_server;
+use benita::services::conductivity::ConductivitySensorService;
 
 use clap::{App, Arg};
 
@@ -24,26 +25,51 @@ fn parse_cli_arguments() -> Result<()> {
             "Benita IoT. A response service for electrical conductivity data.",
         )
         .arg(
-            Arg::with_name("rep-url")
-                .short("r")
-                .long("rep-url")
-                .value_name("REP_URL")
+            Arg::with_name("URL")
                 .help("Sets the url for the response server")
                 .takes_value(true)
-                .index(1)
+                .validator(is_url)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("I2C")
+                .help("Sets the path for the I2C sensor.")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("ADDRESS")
+                .help("Sets the I2C sensor address.")
+                .takes_value(true)
                 .required(true),
         )
         .get_matches();
 
     let mut rep_url = String::new();
-
-    if let Some(c) = matches.value_of("rep-url") {
-        rep_url = String::from(c);
+    if let Some(c) = matches.value_of("URL") {
+        rep_url = c.to_string();
     }
 
-    let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+    let mut device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+    if let Some(c) = matches.value_of("I2C") {
+        device_path = c.to_string();
+    }
 
-    let _run = run_conductivity_server(&rep_url, &device_path, EZO_SENSOR_ADDR)?;
+    let mut address = EZO_SENSOR_ADDR;
+    if let Some(c) = matches.value_of("ADDRESS") {
+        address = c.parse()
+            .chain_err(|| "Bad Address")?;
+    }
+
+    // We initialize our service.
+    let mut service = ConductivitySensorService::new(&rep_url, &device_path, address)
+        .chain_err(|| "Could not create Conductivity service")?;
+
+    {
+        // This is the main loop, it will run for as long as the program runs.
+        let _listen = service.listen()
+            .chain_err(|| "Conductivity service stopped listening")?;
+    }
 
     // Never reach this line...
     Ok(())
