f75085e0d2c50a0747790d36ce228a0752155a02
Joaquin Rosales <globojorro@gmail.com>
2017-10-08 23:11:01 -0500

    refactor pH network client

---
+++ src/network/ph/client.rs
@@ -0,0 +1,35 @@
+//! Client for pH sensing.
+use errors::*;
+
+use neuras;
+
+
+/// Creates a client for network requests of the ph sensor.
+network_socket!(PhClient, "Socket that communicates with the pH sensor.");
+
+impl PhClient {
+    pub fn get_output_params(&self) -> Result<String> {
+        let _send = self.send("get_params".as_bytes())?;
+        let response = self.recv().chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
+    }
+
+    pub fn send_compensate(&self, t: f64) -> Result<String> {
+        let compensate = format!("calibrate {:.*}", 3, t);
+        let _send = self.send(compensate.as_bytes())?;
+        let response = self.recv().chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
+    }
+
+    pub fn send_read(&self) -> Result<String> {
+        let _send = self.send("read".as_bytes())?;
+        let response = self.recv().chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
+    }
+
+    pub fn send_sleep(&self) -> Result<String> {
+        let _send = self.send("sleep".as_bytes())?;
+        let response = self.recv().chain_err(|| ErrorKind::CommandResponse)?;
+        Ok(response)
+    }
+}
