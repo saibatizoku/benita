48f9b6773e41f6790fa9b0de6608a2da5922ee34
Joaquin Rosales <globojorro@gmail.com>
2017-10-12 23:44:26 -0500

    upgrade examples to use services

---
+++ examples/ph-command-server.rs
@@ -0,0 +1,91 @@
+//! Allows for remote command of the PH EZO chip, exposing a limited API.
+//!
+//! This server binds to the `REP_URL` argument, expected from the command line.
+
+// error-chain recurses deeply
+#![recursion_limit = "1024"]
+
+extern crate benita;
+extern crate clap;
+
+use benita::cli::shared::is_url;
+use benita::config::{SensorConfig, SocketConfig};
+use benita::errors::*;
+use benita::services::ph::PhSensorService;
+
+use clap::{App, Arg};
+
+// Parse the command-line arguments and execute.
+fn parse_cli_arguments() -> Result<()> {
+    let matches = App::new("benita-ph-network-service")
+        .version("0.1.0")
+        .author("Joaquin R. <globojorro@gmail.com>")
+        .about("Benita IoT. A network service for pH data.")
+        .arg(
+            Arg::with_name("URL")
+                .help("Sets the url for the response server")
+                .takes_value(true)
+                .validator(is_url)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("I2C")
+                .help("Sets the path for the I2C sensor.")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("ADDRESS")
+                .help("Sets the I2C sensor address.")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let mut socket_cfg = SocketConfig::default();
+
+    if let Some(c) = matches.value_of("URL") {
+        socket_cfg.url = c;
+    }
+
+    let mut sensor_cfg = SensorConfig::default();
+
+    if let Some(c) = matches.value_of("I2C") {
+        sensor_cfg.path = c;
+    }
+
+    if let Some(c) = matches.value_of("ADDRESS") {
+        sensor_cfg.address = c.parse().chain_err(|| "Bad Address")?;
+    }
+
+    // We initialize our service.
+    let mut service = PhSensorService::new(socket_cfg, sensor_cfg)
+        .chain_err(|| "Could not create pH service")?;
+
+    {
+        // This is the main loop, it will run for as long as the program runs.
+        let _listen = service
+            .listen()
+            .chain_err(|| "pH service stopped listening")?;
+    }
+
+    // Never reach this line...
+    Ok(())
+}
+
+fn main() {
+    if let Err(ref e) = parse_cli_arguments() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+        ::std::process::exit(1);
+    }
+}
