eb63d817b6482196221ec3ef67293959a1cb20a0
Joaquin Rosales <globojorro@gmail.com>
2017-11-16 22:05:14 -0600

    utilities for creating bind/connect rep/req

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,12 +1,35 @@
 //! Shared useful utility functions.
 use errors::*;
 use neuras::zmq::Socket;
-use neuras::utils::{bind_socket, connect_socket, create_context, zmq_rep};
+use neuras::utils::{bind_socket, connect_socket, create_context, zmq_rep, zmq_req};
 
 /// simple atof conversion.
 pub fn atof(s: &str) -> Result<f64> {
     let _float = s.parse().chain_err(|| ErrorKind::NumberParse)?;
     Ok(_float)
+}
+
+/// create a REQUESTER socket bound to the specified URL.
+pub fn create_and_bind_requester(url: &str) -> Result<Socket> {
+    // We start our ZMQ context.
+    let context = create_context();
+    // We configure our socket as REQ, for accepting requests
+    let requester = zmq_req(&context)?;
+    // We bind our socket to URL.
+    let _bind_socket = bind_socket(&requester, url).chain_err(|| "problems binding to socket")?;
+    Ok(requester)
+}
+
+/// create a REQUESTER socket connected to the specified URL.
+pub fn create_and_connect_requester(url: &str) -> Result<Socket> {
+    // We start our ZMQ context.
+    let context = create_context();
+    // We configure our socket as REQ, for accepting requests
+    let requester = zmq_req(&context)?;
+    // We bind our socket to URL.
+    let _bind_socket =
+        connect_socket(&requester, url).chain_err(|| "problems connecting to socket")?;
+    Ok(requester)
 }
 
 /// create a RESPONDER socket bound to the specified URL.
