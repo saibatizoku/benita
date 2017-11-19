fb3c62312484d1c4fc20ea487be633f7d25d5c56
Joaquin Rosales <globojorro@gmail.com>
2017-11-18 18:46:26 -0600

    refactor into ph::network

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -7,19 +7,16 @@ mod traits;
 // Common network items
 pub mod common;
 
-pub mod ph;
 pub mod temperature;
 
 pub mod errors {
     use super::common;
-    use super::ph;
     use super::temperature;
     use neuras;
 
     error_chain! {
         links {
             Common(common::errors::Error, common::errors::ErrorKind);
-            Ph(ph::errors::Error, ph::errors::ErrorKind);
             Temperature(temperature::errors::Error, temperature::errors::ErrorKind);
             // external crate error-chains
             Neuras(neuras::errors::Error, neuras::errors::ErrorKind);
@@ -28,6 +25,7 @@ pub mod errors {
 }
 
 pub use conductivity::network as conductivity;
+pub use ph::network as ph;
 
 /// Important traits.
 pub use self::common::{Endpoint, SocketReply, SocketRequest};
--- src/ph/mod.rs
+++ src/ph/mod.rs
@@ -3,6 +3,7 @@
 /// pH `Error`, and `ErrorKind` definitions.
 pub mod errors {
     use super::device;
+    use super::network;
     use ezo_common;
 
     error_chain! {
@@ -13,6 +14,7 @@ pub mod errors {
         }
         links {
             PhDevice(device::errors::Error, device::errors::ErrorKind);
+            PhNetwork(network::errors::Error, network::errors::ErrorKind);
             // error chains from other crates
             EzoSensorDevice(ezo_common::errors::Error, ezo_common::errors::ErrorKind);
         }
@@ -21,6 +23,7 @@ pub mod errors {
 
 mod api;
 pub mod device;
+pub mod network;
 
 pub use self::api::*;
 pub use self::device::PhSensor;
