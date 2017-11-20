0dda048ab74b4569c40d2c83cac4e2c8d6de8d1a
Joaquin Rosales <globojorro@gmail.com>
2017-11-19 22:31:51 -0600

    refactor ph command/response to upper module

---
+++ src/ph/command.rs
@@ -0,0 +1,12 @@
+//! Commands from EZO PH chipset.
+pub use ezo_ph::command::Baud;
+pub use ezo_ph::command::Command;
+pub use ezo_ph::command::{CalibrationClear, CalibrationHigh, CalibrationLow, CalibrationMid,
+                          CalibrationState};
+pub use ezo_ph::command::{CompensatedTemperatureValue as CompensationGet, DeviceAddress,
+                          TemperatureCompensation as CompensationSet};
+pub use ezo_ph::command::{DeviceInformation, Factory, Find, Reading, Sleep, Status};
+pub use ezo_ph::command::{Export, ExportInfo, Import};
+pub use ezo_ph::command::{LedOff, LedOn, LedState};
+pub use ezo_ph::command::{ProtocolLockDisable, ProtocolLockEnable, ProtocolLockState};
+pub use ezo_ph::command::Slope;
---
+++ src/ph/response.rs
@@ -0,0 +1,4 @@
+//! Responses from EZO PH chipset.
+pub use ezo_ph::response::{CalibrationStatus, CompensationValue, DeviceInfo, DeviceStatus,
+                           Exported, ExportedInfo, LedStatus, ProbeSlope, ProtocolLockStatus,
+                           SensorReading};
