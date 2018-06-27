d42022b43abdb91181345e2af08442ed1c3fa5c1
Joaquín Rosales <globojorro@gmail.com>
2018-06-27 12:31:04 -0500

    cargo update with url=1.7

--- src/errors.rs
+++ src/errors.rs
@@ -1,64 +1,85 @@
 //! Library Error, and ErrorKind definitions.
-use super::utilities;
+use std::result;
+use std::fmt::{self, Display};
+use failure::{Backtrace, Context, Fail};
 
-use neuras;
+pub use failure::ResultExt;
 
-error_chain! {
-    errors {
-        AddressParse {
-            description ("could not parse address")
-        }
-        CommandParse {
-            description ("could not parse command")
-        }
-        CommandRequest {
-            description ("command request failed")
-        }
-        CommandReply {
-            description ("command reply failed")
-        }
-        CommandResponse {
-            description ("command response failed")
-        }
-        ConfigParse {
-            description ("could not parse configuration file")
-        }
-        SocketBind {
-            description ("socket could not bind to the network URL")
-        }
-        SocketCreate {
-            description ("the socket couldn't be created")
-        }
-        SocketConnect {
-            description ("socket could not connect to the network URL")
-        }
-        SocketSend {
-            description ("message could not be sent to the network")
-        }
-        SocketReceive {
-            description ("message could not be received from the network")
-        }
-        Neurotic {
-            description ("our network has gone neurotic")
-        }
-        NumberParse {
-            description ("this is not a number")
-        }
-        RequestParse {
-            description ("could not parse request")
-        }
-        ResponseParse {
-            description ("could not parse response")
-        }
-        SensorTrouble {
-            description ("trouble with the sensor")
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+#[derive(Copy, Clone, Eq, Debug, Fail, PartialEq)]
+pub enum ErrorKind {
+    #[fail(display = "could not parse address")]
+    AddressParse,
+    #[fail(display = "could not parse command")]
+    CommandParse,
+    #[fail(display = "command request failed")]
+    CommandRequest,
+    #[fail(display = "command reply failed")]
+    CommandReply,
+    #[fail(display = "command response failed")]
+    CommandResponse,
+    #[fail(display = "could not parse configuration file")]
+    ConfigParse,
+    #[fail(display = "socket could not bind to the network URL")]
+    SocketBind,
+    #[fail(display = "the socket couldn't be created")]
+    SocketCreate,
+    #[fail(display = "socket could not connect to the network URL")]
+    SocketConnect,
+    #[fail(display = "message could not be sent to the network")]
+    SocketSend,
+    #[fail(display = "message could not be received from the network")]
+    SocketReceive,
+    #[fail(display = "our network has gone neurotic")]
+    Neurotic,
+    #[fail(display = "this is not a number")]
+    NumberParse,
+    #[fail(display = "could not parse request")]
+    RequestParse,
+    #[fail(display = "could not parse response")]
+    ResponseParse,
+    #[fail(display = "trouble with the sensor")]
+    SensorTrouble,
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn kind(&self) -> ErrorKind {
+        *self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
         }
     }
-    links {
-        // module error-chains
-        Utilities(utilities::errors::Error, utilities::errors::ErrorKind);
+}
 
-        // external crate error-chains
-        Neuras(neuras::errors::Error, neuras::errors::ErrorKind);
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner: inner }
     }
 }
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,72 +1,55 @@
 //! Shared useful utility functions.
-pub mod errors {
-    use neuras;
-
-    error_chain! {
-        errors {
-            NumberParse {
-                description ("this is not a number")
-            }
-        }
-        links {
-            // external crate error-chains
-            Neuras(neuras::errors::Error, neuras::errors::ErrorKind);
-        }
-    }
-}
-
-use self::errors::*;
-use neuras::zmq::Socket;
-use neuras::utils::{bind_socket, connect_socket, create_context, zmq_rep, zmq_req};
+use super::errors::*;
+use zmq::{Context, Socket, SocketType};
 
 /// simple atof conversion.
 pub fn atof(s: &str) -> Result<f64> {
-    let _float = s.parse().chain_err(|| ErrorKind::NumberParse)?;
+    let _float = s.parse::<f64>().context(ErrorKind::NumberParse)?;
     Ok(_float)
 }
 
 /// create a REQUESTER socket bound to the specified URL.
 pub fn create_and_bind_requester(url: &str) -> Result<Socket> {
     // We start our ZMQ context.
-    let context = create_context();
+    let context = Context::new();
     // We configure our socket as REQ, for accepting requests
-    let requester = zmq_req(&context)?;
+    let requester = context.socket(SocketType::REQ).context(ErrorKind::SocketCreate)?;
     // We bind our socket to URL.
-    let _bind_socket = bind_socket(&requester, url).chain_err(|| "problems binding to socket")?;
+    let _bind_socket = requester.bind(url).context(ErrorKind::SocketBind)?;
     Ok(requester)
 }
 
 /// create a REQUESTER socket connected to the specified URL.
 pub fn create_and_connect_requester(url: &str) -> Result<Socket> {
     // We start our ZMQ context.
-    let context = create_context();
+    let context = Context::new();
     // We configure our socket as REQ, for accepting requests
-    let requester = zmq_req(&context)?;
+    let requester = context.socket(SocketType::REQ).context(ErrorKind::SocketCreate)?;
     // We bind our socket to URL.
     let _bind_socket =
-        connect_socket(&requester, url).chain_err(|| "problems connecting to socket")?;
+        requester.connect(url).context(ErrorKind::SocketConnect)?;
     Ok(requester)
 }
 
 /// create a RESPONDER socket bound to the specified URL.
 pub fn create_and_bind_responder(url: &str) -> Result<Socket> {
     // We start our ZMQ context.
-    let context = create_context();
+    let context = Context::new();
     // We configure our socket as REP, for accepting requests
-    let responder = zmq_rep(&context)?;
+    let responder = context.socket(SocketType::REP).context(ErrorKind::SocketCreate)?;
     // We bind our socket to URL.
-    let _bind_socket = bind_socket(&responder, url).chain_err(|| "problems binding to socket")?;
+    let _bind_socket = responder.bind(url).context(ErrorKind::SocketBind)?;
     Ok(responder)
 }
 
 /// create a RESPONDER socket connected to the specified URL.
 pub fn create_and_connect_responder(url: &str) -> Result<Socket> {
     // We start our ZMQ context.
-    let context = create_context();
+    let context = Context::new();
     // We configure our socket as REP, for accepting requests
-    let responder = zmq_rep(&context)?;
+    let responder = context.socket(SocketType::REP).context(ErrorKind::SocketCreate)?;
     // We bind our socket to URL.
     let _bind_socket =
-        connect_socket(&responder, url).chain_err(|| "problems connecting to socket")?;
+        responder.connect(url).context(ErrorKind::SocketConnect)?;
     Ok(responder)
 }
